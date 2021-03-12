494122d3d67db9d347cd3e3892f286627d73033b
vitaly.tolstikov <vitaly.tolstikov@devexpress.com>
2021-03-12 15:35:57 +0300

    1

---
+++ CS/GridControlCRUDMVVMInfiniteAsyncSource/IssueDataFilterConverter.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Data;
+
+namespace GridControlCRUDMVVMInfiniteAsyncSource {
+    public class IssueDataFilterConverter : ExpressionFilterConverter<IssueData> {
+        protected override void SetUpConverter(GridFilterCriteriaToExpressionConverter<IssueData> converter) {
+            converter.RegisterFunctionExpressionFactory(
+                operatorType: FunctionOperatorType.StartsWith,
+                factory: (string value) => {
+                    var toLowerValue = value.ToLower();
+                    return x => x.ToLower().StartsWith(toLowerValue);
+                });
+        }
+    }
+}
