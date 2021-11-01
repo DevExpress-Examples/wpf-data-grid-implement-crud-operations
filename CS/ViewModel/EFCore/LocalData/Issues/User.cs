f1ed454474f80e0aa745d5e179a12c14d2f1734e
CORP\ploskin.alexander <alexander.ploskin@devexpress.com>
2021-11-01 13:20:29 +0300

    Add ability to undo operations

--- CS/ViewModel/EFCore/LocalData/Issues/User.cs
+++ CS/ViewModel/EFCore/LocalData/Issues/User.cs
@@ -1,10 +1,22 @@
+using DevExpress.Mvvm;
 using System.Collections.Generic;
 
 namespace EFCoreIssues.Issues {
-    public class User {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public virtual ICollection<Issue> Issues { get; set; }
+    public class User : BindableBase {
+        public int Id { get => GetValue<int>(); set => SetValue(value); }
+        public string FirstName { get => GetValue<string>(); set => SetValue(value); }
+        public string LastName { get => GetValue<string>(); set => SetValue(value); }
+        public virtual ICollection<Issue> Issues { get => GetValue<ICollection<Issue>>(); set => SetValue(value); }
+
+        public User Clone() {
+            return new User() { FirstName = FirstName, Id = Id, Issues = Issues, LastName = LastName };
+        }
+
+        public void CopyTo(User anotherUser) {
+            anotherUser.FirstName = FirstName;
+            anotherUser.Id = Id;
+            anotherUser.Issues = Issues;
+            anotherUser.LastName = LastName;
+        }
     }
 }
--- CS/ViewModel/EFCore/LocalData/MainViewModel.cs
+++ CS/ViewModel/EFCore/LocalData/MainViewModel.cs
@@ -1,32 +1,82 @@
 using DevExpress.Mvvm;
+using EFCoreIssues.Issues;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace EFCoreIssues {
     public class MainViewModel : ViewModelBase {
         EFCoreIssues.Issues.IssuesContext _Context;
-        System.Collections.Generic.IList<EFCoreIssues.Issues.User> _ItemsSource;
+        ObservableCollection<EFCoreIssues.Issues.User> _ItemsSource;
 
-        public System.Collections.Generic.IList<EFCoreIssues.Issues.User> ItemsSource
+        public ObservableCollection<EFCoreIssues.Issues.User> ItemsSource
         {
             get
             {
                 if(_ItemsSource == null && !IsInDesignMode) {
                     _Context = new EFCoreIssues.Issues.IssuesContext();
-                    _ItemsSource = _Context.Users.ToList();
+                    _ItemsSource = new ObservableCollection<User>(_Context.Users);
                 }
                 return _ItemsSource;
             }
         }
+
+        void RemoveItem(User item) {
+            ItemsSource.Remove(item);
+            _Context.Users.Remove(item);
+            _Context.SaveChanges();
+        }
+
+        void InsertItem(int position, User item) {
+            ItemsSource.Insert(position, item);
+            _Context.Users.Add(item);
+            _Context.SaveChanges();
+        }
+
+        void ApplyEditingCache(User item) {
+            editingCache.CopyTo(item);
+            _Context.SaveChanges();
+        }
+
+        Action undoAction;
+        User editingCache;
+
+
+        [DevExpress.Mvvm.DataAnnotations.Command]
+        public void Undo() {
+            undoAction?.Invoke();
+            undoAction = null;
+        }
+
+        public bool CanUndo() {
+            return undoAction != null;
+        }
+
+        [DevExpress.Mvvm.DataAnnotations.Command]
+        public void StartRowEdit(DevExpress.Mvvm.Xpf.RowEditStartedArgs args) {
+            if(args.Item != null) {
+                editingCache = ((User)args.Item).Clone();
+            }
+        }
+
         [DevExpress.Mvvm.DataAnnotations.Command]
         public void ValidateRow(DevExpress.Mvvm.Xpf.RowValidationArgs args) {
             var item = (EFCoreIssues.Issues.User)args.Item;
-            if(args.IsNewItem)
+            
+            undoAction = args.IsNewItem ? new Action(() => RemoveItem(item)) : new Action(() => ApplyEditingCache(item));
+
+            if(args.IsNewItem) {
                 _Context.Users.Add(item);
+            }
             _Context.SaveChanges();
         }
+
         [DevExpress.Mvvm.DataAnnotations.Command]
         public void ValidateRowDeletion(DevExpress.Mvvm.Xpf.ValidateRowDeletionArgs args) {
             var item = (EFCoreIssues.Issues.User)args.Items.Single();
+
+            undoAction = new Action(() => InsertItem(args.SourceIndexes.Single(), item.Clone()));
+
             _Context.Users.Remove(item);
             _Context.SaveChanges();
         }
